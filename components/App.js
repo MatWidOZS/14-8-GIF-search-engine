e826ae70a492721867492ce32d6be7d5a9514813
Mateusz Widerski <mat.widerski@gmail.com>
2018-05-12 15:26:08 +0200

    Delete comments

--- components/App.js
+++ components/App.js
@@ -7,51 +7,35 @@ App = React.createClass({
 		}
 	},
 
-	handleSearch: function(searchingText) {	// 1.
+	handleSearch: function(searchingText) {
 		this.setState({
-			loading: true	// 2.
+			loading: true
 		});
-		this.getGif(searchingText, function(gif) {	// 3.
-			this.setState({	// 4
-				loading: false,	// a
-				gif: gif,	//b
-				searchingText: searchingText	//c
+		this.getGif(searchingText, function(gif) {
+			this.setState({
+				loading: false,
+				gif: gif,
+				searchingText: searchingText
 			});
 		}.bind(this));
 	},
 
-	/*Algorytm postępowania dla tej metody jest następujący:
-	1. pobierz na wejściu wpisywany tekst,,
-	2. zasygnalizuj, że zaczął się proces ładowania,
-	3. Rozpocznij pobieranie gifa,
-	4. Na zakończenie pobierania:
-		a. przestań sygnalizować ładowanie,
-		b. ustaw nowego gifa z wyniku pobierania,
-		c. ustaw nowy stan dla wyszukiwanego tekstu.*/
-
-	getGif: function(searchingText, callback) {	 // 1.
-		var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;	//2.
-		var xhr = new XMLHttpRequest();	//3.
+	getGif: function(searchingText, callback) {
+		var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+		var xhr = new XMLHttpRequest();
 		xhr.open('GET', url);
 		xhr.onload = function() {
 			if (xhr.status === 200) {
-				var data = JSON.parse(xhr.responseText).data;	//4.
-				var gif = {	//5.
+				var data = JSON.parse(xhr.responseText).data;
+				var gif = {
 					url: data.fixed_width-downsampled_url,
 					sourceUrl: data.url
 				};
-				callback(gif);	//6.
+				callback(gif);
 			}
 		};
 		xhr.send();
 	},
-
-	/*1. Na wejście metody getGif przyjmujemy dwa parametry: wpisywany tekst (searchingText) i funkcję, która ma się wykonać po pobraniu gifa (callback),
-	2. Konstruujemy adres URL dla API Giphy (pełną dokumentację znajdziesz pod tym adresem),
-	3. Wywołujemy całą sekwencję tworzenia zapytania XHR do serwera i wysyłamy je.
-	4. W obiekcie odpowiedzi mamy obiekt z danymi. W tym miejscu rozpakowujemy je sobie do zmiennej data, aby nie pisać za każdym razem response.data.
-	5. Układamy obiekt gif na podstawie tego co otrzymaliśmy z serwera
-	6. Przekazujemy obiekt do funkcji callback, którą przekazaliśmy jako drugi parametr metody getGif.*/
 
 	render: function() {
 
